39734f737e4317b49481000601545aa65b06c362
SecurityRaven <aggeloskarapatakis@gmail.com>
2022-06-13 18:28:06 +0300

    Uploaded Sources

---
+++ SayTheSong/SayTheSong/SayTheSong.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using Daniel15.Sharpamp;
+using SpeechLib;
+namespace SayTheSong
+{
+    public class SayTheSong : Daniel15.Sharpamp.GeneralPlugin
+    {
+        public override string Name
+        {
+            get { return "SayTheSong"; }
+        }
+
+        public override void Initialize()
+        {
+            Winamp.SongChanged += Winamp_SongChanged;
+        }
+
+
+        public override void Config()
+        {
+
+            MessageBox.Show("...");
+        }
+        void Winamp_SongChanged(object sender, Daniel15.Sharpamp.SongChangedEventArgs e)
+        {
+            SpVoice spVoice = new SpVoice();
+            if (e.Song.HasMetadata == true)
+            {
+                spVoice.Speak("Now Playing" + e.Song.Title + "With" + e.Song.Artist, SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak);
+            }
+            else
+            {
+                spVoice.Speak("Now Playing" + e.Song.Title, SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak);
+            }
+
+        }
+    }
+}
